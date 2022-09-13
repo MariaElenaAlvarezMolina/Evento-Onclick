0929b9a4b53ff12281e3c1745a40a79eaa09d5cc
María Elena Álvarez Molina <nutri.mariaelenaalvarez@outlook.com>
2022-09-13 00:00:37 -0400

    Evento Onclick

---
+++ script.js
@@ -0,0 +1,9 @@
+function cambieNombre(boton1) {
+    boton1.innerText = "Logout";
+}
+function elimine(boton3) {
+    boton3.remove();
+}
+function click_editar() {
+    alert("Ninka was liked");
+}
